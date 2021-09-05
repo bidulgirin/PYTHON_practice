c03c84a8999881ef698c41de943648b29f9e79c3
bidulgirin <de05164@naver.com>
2021-09-05 14:51:33 +0900

    make_character

--- 3_main_sprite.py
+++ 3_main_sprite.py
@@ -13,6 +13,13 @@ pygame.display.set_caption("nado game")
 # 배경 이미지 불러오기
 background = pygame.image.load("./img/background.png")
 
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("./img/character.png")
+character_size = character.get_rect().size
+character_width = character_size[0] # 캐릭터의 가로크기
+character_height = character_size[1] # 캐릭터의 세로크기
+character_x_pos = screen_width / 2  - character_width / 2 # 화면 가로의 절반 크기에 해당하는 곳에 위치
+character_y_pos = screen_height - character_height   # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
 # 이벤트 루프
 running = True # 게임이 진행중인가
 while running:
@@ -22,6 +29,7 @@ while running:
     
     screen.blit(background, (0, 0)) #배경그리기
 
+    screen.blit(character, (character_x_pos, character_y_pos))
     pygame.display.update() # 게임화면을 다시 그리기
 # pygame 종료
 pygame.quit()
